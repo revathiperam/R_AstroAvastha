f04c3e1a26c6febc5ca83344eeec599445f7abcf
peramrevathi <peramrevathi2002@gmail.com>
2024-12-30 16:14:49 +0530

    worked on the navigations

--- navigations/DrawerNavigation.jsx
+++ navigations/DrawerNavigation.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native'; // Import NavigationContainer
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Header from '../components/Header';
 import HomeScreen from '../src/Home';
 import DiscoverScreen from '../src/Discover';
-import { DrawerActions } from '@react-navigation/native';
+
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
     return (
-
-        <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{
-                    header: (props) => <Header {...props} />,
-                }}
-            />
-            <Drawer.Screen name="Discover" component={DiscoverScreen} />
-        </Drawer.Navigator>
-
-
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home">
+                <Drawer.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        header: (props) => <Header {...props} />,
+                    }}
+                />
+                <Drawer.Screen name="Discover" component={DiscoverScreen} />
+            </Drawer.Navigator>
+        </NavigationContainer>
     );
 };
 
